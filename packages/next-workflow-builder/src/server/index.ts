9573c9ad97d5958a64b68140d5cabe2a38618602
David Sanchez <david38sanchez@gmail.com>
2026-02-14 09:54:13 -0500

    Remove old API endpoints files

--- packages/next-workflow-builder/src/server/index.ts
+++ packages/next-workflow-builder/src/server/index.ts
@@ -1,6 +1,5 @@
-export { default, type WorkflowConfig } from './plugin.js';
-export { createWorkflowApiHandler } from './api/workflows.js';
-export type { WorkflowApiHandlerOptions } from './api/types.js';
-export { createExecutionHandler } from './api/execute.js';
-export { generateApiRoute, resolveAppDir } from './generate-route.js';
-export * as schema from './db/schema.js';
+export { default, type WorkflowConfig } from "./plugin.js";
+export { createWorkflowApiHandler } from "./api/workflows.js";
+export type { WorkflowApiHandlerOptions } from "./api/types.js";
+export { generateApiRoute, resolveAppDir } from "./generate-route.js";
+export * as schema from "./db/schema.js";
