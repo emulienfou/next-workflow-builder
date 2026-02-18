0f1849dd555423c11ddb86ee63859678648be219
David Sanchez <david38sanchez@gmail.com>
2026-02-18 09:18:47 -0500

    Add missing official plugins

--- plugins/communication/slack/icon.tsx
+++ plugins/communication/slack/icon.tsx
@@ -2,7 +2,7 @@ export function SlackIcon({ className }: { className?: string }) {
   return (
     <svg
       aria-label="Slack"
-      className={className}
+      className={ className }
       viewBox="0 0 2447.6 2452.5"
       xmlns="http://www.w3.org/2000/svg"
     >
@@ -28,4 +28,3 @@ export function SlackIcon({ className }: { className?: string }) {
     </svg>
   );
 }
-
