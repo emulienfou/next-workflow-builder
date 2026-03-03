79f10bab6ffde6fc7cc652125ffb1f179f255c21
David Sanchez <david38sanchez@gmail.com>
2026-03-02 22:13:34 -0500

    Add missing metadata

--- packages/next-workflow-builder/src/server/index.ts
+++ packages/next-workflow-builder/src/server/index.ts
@@ -1,6 +1,7 @@
 export { ResultComponentProps } from "./types";
 export { getErrorMessageAsync } from "../client/lib/utils";
 export { fetchCredentials } from "./lib/credential-fetcher";
+export { generateWorkflowMetadata } from "./lib/metadata";
 export { getErrorMessage } from "./lib/utils";
 export { discoverPlugins } from "../plugins/discover";
 export { withStepLogging, type StepInput } from "./lib/steps/step-handler";
